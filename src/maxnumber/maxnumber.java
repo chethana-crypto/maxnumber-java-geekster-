8e716f81e72489c3e63ca783276741c0cdb6d6fd
chethana-crypto <69971162+chethana-crypto@users.noreply.github.com>
2021-09-12 15:07:17 +0530

    maxnumber
    this is a java program to find  the largest number from two numbers

---
+++ src/maxnumber/maxnumber.java
@@ -0,0 +1,12 @@
+package maxnumber;
+
+public class maxnumber {
+ public static void main(String args[]) {
+	 int a=50, b=70 ;
+	 if(a > b ) {
+		 System.out.println("a is greater than b that is " +a);
+	 }else {
+		 System.out.println("b is greater than a that is " +b); 
+	 }
+  }
+}
